99f5af0e31adf2faeb86d528e44d7c41bacc9b83
wandyezj <wandyezj@gmail.com>
2021-06-02 14:19:29 -0700

    Update format-md-table.py

--- format-md-table.py
+++ format-md-table.py
@@ -8,7 +8,7 @@ import sys
 
 arguments = sys.argv[1:]
 
-print(arguments)
+print("arguments: {}".format(arguments))
 if len(arguments) == 1:
     file = arguments[0]
 else:
@@ -23,56 +23,101 @@ def read(file):
     return data
 
 
-data = read(file)
-
 #print(data)
 
-data = data.strip()
-lines = data.split("\n")
-
-# figure out longest lengths
-longest_column = []
-
-line_columns = []
-
-for line in lines:
-    columns = line.split("|")
-    line_columns.append(columns)
-    for i in range(len(columns)):
-        column = columns[i].strip()
-
-        while len(longest_column) <= i:
-                   longest_column.append(0)
-
-        column_length = len(column)
-
-        if column_length >  longest_column[i]:
-            longest_column[i] = column_length
-
-print(longest_column)
+def tableRowCol(data):
+    lines = data.strip().split("\n")
+    rows = []
+    for line in lines:
+        row = []
+        cols = line.split("|")
+        for col in cols:
+            row.append(col.strip())
+        rows.append(row)
+    return rows
 
 
-# format each line
-rows = []
-for line in line_columns:
-    row = []
-    for i in range(len(line)):
-        column = line[i].strip()
-        size = longest_column[i]
+def orderTableCol(row_col, order):
+    new_row_col = []
 
-        s = column + " " * (size -len(column))
-        row.append(s)
+    for row in row_col:
+        new_row = []
+        for i in order:
+            col = row[i]
+            new_row.append(col)
+        new_row_col.append(new_row)
 
-    rows.append(row)
+    return new_row_col
+    
 
-
-# join it all together
-s = ""
-for row in rows:
-    s += "| ".join(row) + "\n"
+def formatTable(row_col):
+    # figure out longest lengths
+    longest_column = []
 
 
+    for row in row_col:
+        columns = row
+        for i in range(len(columns)):
+            column = columns[i].strip()
+
+            while len(longest_column) <= i:
+                       longest_column.append(0)
+
+            column_length = len(column)
+
+            if column_length >  longest_column[i]:
+                longest_column[i] = column_length
+
+    #print(longest_column)
+
+
+    # format each line
+    rows = []
+    for cols in row_col:
+        row = []
+        for i in range(len(cols)):
+            column = cols[i]
+            size = longest_column[i]
+
+            s = column + " " * (size - len(column))
+            row.append(s)
+
+        rows.append(row)
+
+
+    # join it all together
+    s = ""
+    for row in rows:
+        #print(row)
+        s += "| ".join(row) + "\n"
+
+
+    return s
+
+
+
+data = read(file)
+
+#print("data")
+#print(data)
+row_col = tableRowCol(data)
+
+#print("row_col")
+#print(row_col)
+
+
+# reorder
+order = [0,1,2,3,4,5,6]
+order = [0,1, 6, 2,3,4,5, 7, 8]
+# will fail if not everything has the same number of columns
+row_col = orderTableCol(row_col, order)
+
+#print()
+print("Format Table")
+print("\n\n\n")
+s = formatTable(row_col)
 print(s)
+
 
 
 
