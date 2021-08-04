cc226cc42cec832e8d3f3d7e24c50e9ef4cc64fd
wandyezj <wandyezj@gmail.com>
2021-08-04 20:59:50 +0000

    organize

---
+++ playground/node/hello.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+
+function variables() {
+    const name = "name";
+
+    console.log("hello: " + name);
+}
+
+variables();
---
+++ versions.sh
@@ -0,0 +1,19 @@
+# check versions of relevant programs
+# chmod 777 this file
+# run with ./versions
+
+echo
+echo node --version
+node --version
+
+echo
+echo npm --version
+npm --version
+
+echo
+echo git --version
+git --version
+
+echo
+echo python --version
+python --version
