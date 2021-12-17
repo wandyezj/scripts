53ddabd871e59d177cad6c97b00e39d7d56dd14f
wandyezj <wandyezj@gmail.com>
2021-12-16 21:22:38 -0800

    Update lib.py

--- AdventOfCode2021/lib.py
+++ AdventOfCode2021/lib.py
@@ -6,14 +6,18 @@ def flip_bits(n):
 
 # grid[y][x]
 # grid whe max y = my and max x = mx
-def make_grid(mx, my):
+def make_grid(mx, my, value=0):
     grid = []
     for x in range(mx):
         row = []
         for y in range(my):
-            row.append(0)
+            row.append(value)
         grid.append(row)
     return grid
+
+# make empty grid same size as the passed in grid
+def make_empty_grid(grid, value = 0):
+    return make_grid(len(grid[0]), len(grid), value)
 
 # grid[y][x]
 # mx and my are the maximum x and y value that can be passed to the grid `<=`
@@ -53,6 +57,12 @@ grid_squares_vertical_horizontal = grid_squares_vertical + grid_squares_horizont
 grid_squares_diagonal = [grid_square_up_right, grid_square_up_left, grid_square_down_right, grid_square_down_left]
 grid_squares = grid_squares_vertical_horizontal  + grid_squares_diagonal
 
+# points generated from offsets of x and y that are within grid bounds
+def in_bound_offset_points(grid, x,y, offsets):
+    points = list(map(lambda p : (x + p[0], y + p[1]), offsets))
+    ok = list(filter(lambda p: in_bounds(grid, p[0], p[1]), points))
+    return ok
+
 def to_ints(line):
     l = list(line)
     return list(map(lambda x: int(x), l))
@@ -63,6 +73,14 @@ def get_int_grid(data):
         to_ints, 
         data.split("\n")))
     return grid
+
+def print_grid(grid):
+    s = ""
+    for y in grid:
+        s += " ".join(list(map(lambda s: str(s), y))) + "\n"
+
+    print(s)
+
 
 def read_file(file):
     f = open(file)
